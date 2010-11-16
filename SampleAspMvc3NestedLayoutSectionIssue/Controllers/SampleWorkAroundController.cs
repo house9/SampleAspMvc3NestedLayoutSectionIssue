27016beeaf00b9b8085ceda29fd8fb41d5bf5b29
Jesse House <jesse.house@yahoo.com>
2010-11-16 09:03:35 -0800

    added workaround code supplied by Imran Baloch

---
+++ SampleAspMvc3NestedLayoutSectionIssue/Controllers/SampleWorkAroundController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleAspMvc3NestedLayoutSectionIssue.Controllers
+{
+    public class SampleWorkAroundController : Controller
+    {
+        //
+        // GET: /Sample/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
